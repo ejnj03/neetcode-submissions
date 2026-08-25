63c2012a084f33e9d0548d4635b352a6d32737aa
ejnj03 <108601601+ejnj03@users.noreply.github.com>
2026-08-25 16:17:32 -0700

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,33 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        bucket sort
+        - can be not stable
+        - have a defined range of buckets (max number of occurences = length)
+        
+        - get counts of each first (same as heap approach)
+        - then classify into buckets
+
+        - both counter (number of unique chars) and buckets (range of max freq) is O(N), adding to res is O(K)
+        adding to each is also O(N), same for extracting top K
+
+        **lists are by reference**
+        """
+
+        counter = defaultdict(int)
+        
+        for num in nums:
+            counter[num]+=1
+
+
+        buckets = [[] for _ in range (len(nums) + 1)] #0 to len(nums)
+        
+        for n, freq in counter.items():
+            buckets[freq].append(n)
+
+        res = []
+        for freq in range(len(nums), -1, -1):
+            if len(res) == k: return res
+            
+            res.extend(buckets[freq])
+
