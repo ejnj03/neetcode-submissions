4a0d58606753d1884ff81ba23b13711069713c2f
ejnj03 <108601601+ejnj03@users.noreply.github.com>
2026-08-28 14:20:44 -0700

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        p1, p2 = 0, len(s) - 1
+        while p1 <= p2:
+            #print(re.match(r"[A-Za-z0-9]", s[p2]))
+            while re.match(r"[A-Za-z0-9]", s[p1]) is None and p1 < p2:
+                p1 += 1
+            while re.match(r"[A-Za-z0-9]", s[p2]) is None and p2 > p1:
+                p2 -= 1
+            s1, s2 = s[p1].lower(), s[p2].lower()
+            #print(s1, s2)
+            if s1 != s2: return False
+            p1, p2 = p1 + 1, p2 - 1
+        return True 
